52c3bc9f14dc1f21472fca60bf351c82526575f8
camenduru <camenduru@gmail.com>
2023-03-13 07:59:06 +0300

    🏛

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
-import { computed, reactive, ref } from 'vue'
-import { useFetch } from '@vueuse/core'
+import { computed, reactive, ref } from "vue";
+import { useFetch } from "@vueuse/core";
 
-const url = ref('https://aica.vercel.app/all.json')
-const refetch = ref(false)
-const {
-  data,
-  error,
-  statusCode,
-  isFetching,
-  isFinished,
-  canAbort,
-} = useFetch(url, { refetch }).get()
+const url = ref("https://aica.vercel.app/all.json");
+const refetch = ref(false);
+const { data, error, statusCode, isFetching, isFinished, canAbort } = useFetch(
+  url,
+  { refetch }
+).get();
 const text = reactive({
   isFinished,
   isFetching,
@@ -20,32 +16,40 @@ const text = reactive({
   error,
   data: computed(() => {
     try {
-      return JSON.parse(data.value as string)
-    }
-    catch (e) {
-      return null
+      return JSON.parse(data.value as string);
+    } catch (e) {
+      return null;
     }
   }),
-})
+});
 </script>
 
 <template>
+  <div class="creator-archive">
+    <img src="https://emojicdn.elk.sh/🏛?style=apple" width="42" height="42" />
+    <span style="font-size: 50px;"> AICA </span>
+    <span style="font-size: 14px;">AI Creator Archive</span>
+  </div>
   <masonry-wall v-if="text.data" :items="text.data" :ssr-columns="1" :column-width="350" :gap="10">
     <template #default="{ item, index }">
-      <div>
-        <h1>{{ item.creator_username }}</h1>
-        <img :src="`https://via.placeholder.com/300x300`" />
-        <br>
+      <div class="grid-item">
+        <h2>{{ item.model_name }}</h2>
+        <p>Version: {{ item.model_version }}</p>
+        <img :src="`https://via.placeholder.com/282x300`" />
+        <br />
         <div>
-          <img :src="`https://via.placeholder.com/71x64`" /> <img :src="`https://via.placeholder.com/71x64`" /> <img
-            :src="`https://via.placeholder.com/71x64`" /> <img :src="`https://via.placeholder.com/71x64`" />
+          <img style="padding-right: 2px;" :src="`https://via.placeholder.com/69x69`" />
+          <img style="padding-right: 2px;" :src="`https://via.placeholder.com/69x69`" />
+          <img style="padding-right: 2px;" :src="`https://via.placeholder.com/69x69`" />
+          <img :src="`https://via.placeholder.com/69x69`" />
         </div>
         <a :href="`${item.online[0]}`"><img :src="`https://colab.research.google.com/assets/colab-badge.svg`" /></a>
-        <p>name: {{ item.model_name }}</p>
-        <p>version: {{ item.model_version }}</p>
-        <p>type: {{ item.type[0] }}</p>
-        <p>tokens:</p>
-        <div v-for="(token, token_index) in item.tokens" :key="token_index">{{ token }}</div>
+        <p>Creator: {{ item.creator_username }}</p>
+        <p>Type: {{ item.type[0] }}</p>
+        <span v-if="item.tokens[0]">Tokens: </span>
+        <span v-for="(token, token_index) in item.tokens" :key="token_index">
+          {{ token }}&nbsp;
+        </span>
       </div>
     </template>
   </masonry-wall>
