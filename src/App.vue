1b6bcc7fdc957251de69e7feae5f4336ae1be1cf
camenduru <camenduru@gmail.com>
2023-03-14 09:20:50 +0300

    🚧 still in development

--- src/App.vue
+++ src/App.vue
@@ -29,19 +29,20 @@ const text = reactive({
   <div class="creator-archive">
     <img src="/logo.png" width="42" height="42" />
     <span style="font-size: 50px; color: #E6E6E6;"> AI</span><span style="font-size: 50px; color: hotpink;">CA </span>
-    <a href="https://github.com/camenduru/ai-creator-archive"><span style="font-size: 14px; color: whitesmoke;"> AI
-    </span>
-    <span style="font-size: 14px; color: hotpink;">Creator Archive</span></a> + <a
-    href="https://github.com/camenduru/stable-diffusion-webui-colab"><span
-      style="font-size: 14px; color: whitesmoke;">Stable Diffusion</span> <span
-      style="font-size: 14px; color: hotpink;">WebUI Colab</span></a>
-    <br>
-    <div>
+    <a href="https://github.com/camenduru/ai-creator-archive"><span style="font-size: 12px; color: whitesmoke;"> AI
+      </span>
+      <span style="font-size: 12px; color: hotpink;">Creator Archive</span></a>
+  + <a href="https://github.com/camenduru/stable-diffusion-webui-colab"><span
+      style="font-size: 12px; color: whitesmoke;">Stable Diffusion</span> <span
+      style="font-size: 12px; color: hotpink;">WebUI Colab</span></a>
+  <br>
+  <div>
       <a href="https://github.com/camenduru/stable-diffusion-webui-colab"><img alt="GitHub" src="/github.svg" /></a>&nbsp;
       <a href="https://twitter.com/camenduru"><img alt="Twitter" src="/twitter.svg" /></a>&nbsp;
       <a href="https://discord.gg/k5BwmmvJJU"><img alt="Discord" src="/discord.svg" /></a>&nbsp;
       <br>
-      🚧<span style="font-size: 14px; color: hotpink;"> Still </span><span style="font-size: 14px; color: whitesmoke;">in development</span>
+      🚧<span style="font-size: 14px; color: hotpink;"> Still </span><span style="font-size: 14px; color: whitesmoke;">in
+        development</span>
     </div>
     <br>
     <div>
@@ -58,11 +59,11 @@ const text = reactive({
         <img :src="`${item.images[0]}`" width="288" />
         <br />
         <!-- <div>
-          <img style="padding-right: 3px;" :src="`https://via.placeholder.com/69x69`" />
-          <img style="padding-right: 3px;" :src="`https://via.placeholder.com/69x69`" />
-          <img style="padding-right: 3px;" :src="`https://via.placeholder.com/69x69`" />
-          <img :src="`https://via.placeholder.com/69x69`" />
-        </div> -->
+            <img style="padding-right: 3px;" :src="`https://via.placeholder.com/69x69`" />
+            <img style="padding-right: 3px;" :src="`https://via.placeholder.com/69x69`" />
+            <img style="padding-right: 3px;" :src="`https://via.placeholder.com/69x69`" />
+            <img :src="`https://via.placeholder.com/69x69`" />
+          </div> -->
         <a :href="`${item.online[0]}`"><img src="/lite.svg" /></a>&nbsp;
         <a :href="`${item.online[1]}`"><img src="/stable.svg" /></a>&nbsp;
         <a :href="`${item.online[2]}`"><img src="/nightly.svg" /></a>
