c16dea91567dc319c0e4220b86ca8651e1ca450d
camenduru <camenduru@gmail.com>
2023-03-14 08:58:06 +0300

    🚧 still in development

--- src/App.vue
+++ src/App.vue
@@ -27,27 +27,26 @@ const text = reactive({
 
 <template>
   <div class="creator-archive">
-  <img src="/logo.png" width="42" height="42" />
-  <span style="font-size: 50px; color: #E6E6E6;"> AI</span><span style="font-size: 50px; color: hotpink;">CA </span>
-  <a href="https://github.com/camenduru/ai-creator-archive"><span style="font-size: 14px; color: whitesmoke;"> AI </span>
+    <img src="/logo.png" width="42" height="42" />
+    <span style="font-size: 50px; color: #E6E6E6;"> AI</span><span style="font-size: 50px; color: hotpink;">CA </span>
+    <a href="https://github.com/camenduru/ai-creator-archive"><span style="font-size: 14px; color: whitesmoke;"> AI
+    </span>
     <span style="font-size: 14px; color: hotpink;">Creator Archive</span></a> + <a
-      href="https://github.com/camenduru/stable-diffusion-webui-colab"><span
-        style="font-size: 14px; color: whitesmoke;">Stable Diffusion</span> <span
-        style="font-size: 14px; color: hotpink;">WebUI Colab</span></a>
+    href="https://github.com/camenduru/stable-diffusion-webui-colab"><span
+      style="font-size: 14px; color: whitesmoke;">Stable Diffusion</span> <span
+      style="font-size: 14px; color: hotpink;">WebUI Colab</span></a>
     <br>
     <div>
-      <a href="https://github.com/camenduru/stable-diffusion-webui-colab"><img alt="GitHub"
-          src="/github.svg" /></a>&nbsp;
-      <a href="https://twitter.com/camenduru"><img alt="Twitter"
-          src="/twitter.svg" /></a>&nbsp;
-      <a href="https://discord.gg/k5BwmmvJJU"><img alt="Discord"
-          src="/discord.svg" /></a>&nbsp;
+      <a href="https://github.com/camenduru/stable-diffusion-webui-colab"><img alt="GitHub" src="/github.svg" /></a>&nbsp;
+      <a href="https://twitter.com/camenduru"><img alt="Twitter" src="/twitter.svg" /></a>&nbsp;
+      <a href="https://discord.gg/k5BwmmvJJU"><img alt="Discord" src="/discord.svg" /></a>&nbsp;
+      <span style="font-size: 20px; color: hotpink;">🚧</span><span style="font-size: 18px; color: hotpink;"> Still </span><span style="font-size: 18px; color: whitesmoke;">in development</span>
     </div>
     <br>
     <div>
       <p><img src="/lite.svg" /> has a stable WebUI and stable installed extensions.</p>
       <p><img src="/stable.svg" /> has ControlNet, a stable WebUI, and stable installed extensions.</p>
-      <p><img src="/nightly.svg" />  has ControlNet, the latest WebUI, and daily installed extension updates.</p>
+      <p><img src="/nightly.svg" /> has ControlNet, the latest WebUI, and daily installed extension updates.</p>
     </div>
   </div>
   <masonry-wall v-if="text.data" :items="text.data" :ssr-columns="1" :column-width="350" :gap="10">
@@ -58,11 +57,11 @@ const text = reactive({
         <img :src="`${item.images[0]}`" width="288" />
         <br />
         <!-- <div>
-              <img style="padding-right: 3px;" :src="`https://via.placeholder.com/69x69`" />
-              <img style="padding-right: 3px;" :src="`https://via.placeholder.com/69x69`" />
-              <img style="padding-right: 3px;" :src="`https://via.placeholder.com/69x69`" />
-              <img :src="`https://via.placeholder.com/69x69`" />
-            </div> -->
+                <img style="padding-right: 3px;" :src="`https://via.placeholder.com/69x69`" />
+                <img style="padding-right: 3px;" :src="`https://via.placeholder.com/69x69`" />
+                <img style="padding-right: 3px;" :src="`https://via.placeholder.com/69x69`" />
+                <img :src="`https://via.placeholder.com/69x69`" />
+              </div> -->
         <a :href="`${item.online[0]}`"><img src="/lite.svg" /></a>&nbsp;
         <a :href="`${item.online[1]}`"><img src="/stable.svg" /></a>&nbsp;
         <a :href="`${item.online[2]}`"><img src="/nightly.svg" /></a>
@@ -72,6 +71,7 @@ const text = reactive({
         <span v-for="(token, token_index) in item.tokens" :key="token_index">
           {{ token }}&nbsp;
         </span>
-    </div>
-  </template>
-</masonry-wall></template>
+      </div>
+    </template>
+  </masonry-wall>
+</template>
